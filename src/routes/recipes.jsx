6b819df80c35387a2743c53ccb96458caca58fe6
maciejkudej <maciejkudej765@gmail.com>
2024-02-15 11:48:23 +0100

    Add find recipes by ID

--- src/routes/recipes.jsx
+++ src/routes/recipes.jsx
@@ -1,7 +1,10 @@
 import express from 'express';
 
 import { getRecipesListMainPage } from 'controllers/recipes/mainPageRecipes';
+import { getRecipesById } from 'controllers/recipes/getRecipesById';
 
 const router = express.Router();
 
 router.get('/home', getRecipesListMainPage);
+
+router.get('/recipes/:id', getRecipesById);
