8554ed0a7d43d3dda8ccd688c4473868732ef414
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2024-02-19 19:54:21 +0100

    Update addIngredients.js

--- src/controllers/ingredients/addIngredients.js
+++ src/controllers/ingredients/addIngredients.js
@@ -2,17 +2,18 @@ import Ingredient from 'models/Ingredients';
 
 const addIngretients = async (req, res, next) => {
   const { title, desc, quantity, thumb } = req.body;
+
+  try {
+    await Ingredient.create({
+      title,
+      desc,
+      quantity,
+      thumb,
+    });
+    return res.status(200).json({ body: `${title} added to ingredients` });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
-try {
-  await Ingredient.create({
-    title,
-    desc,
-    quantity,
-    thumb,
-  });
-  return res.status(200).json({ body: `${title} added to ingredients` });
-} catch (error) {
-  console.log(error.message);
-}
 
 export { addIngretients };
