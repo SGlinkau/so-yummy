16b9689d8b15b2be36cd0a9bd183f04b1015c6c3
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2024-02-19 18:20:40 +0100

    Register-SignIn Front
    Created front-end side of Register and SignIn routes, added them to App.jsx

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,41 @@
+import {
+  BoxForAuth,
+  BoxForImage,
+  AuthImage,
+  Container,
+} from 'pages/SignIn/SignIn.styled';
+import registerGirlMob from '../../img/register-mob.png';
+import registerGirlMob2X from '../../img/register-mob@2x.png';
+import registerGirlTab from '../../img/register-tablet.png';
+import registerGirlTab2X from '../../img/register-tablet@2x.png';
+import registerGirlDesk from '../../img/register-desktop.png';
+import registerGirlDesk2X from '../../img/register-desktop@2x.png';
+
+export default function Register() {
+  return (
+    <BoxForAuth>
+      <Container>
+        <BoxForImage>
+          <AuthImage alt="girl">
+            <source
+              media="(min-width: 1440px)"
+              srcSet={`${registerGirlDesk}, ${registerGirlDesk2X} 2x`}
+            />
+            <source
+              media="(min-width: 768px)"
+              srcSet={`${registerGirlTab},
+                        ${registerGirlTab2X} 2x`}
+            />
+            <img
+              src={registerGirlDesk}
+              srcSet={`${registerGirlMob},
+                        ${registerGirlMob2X} 2x`}
+              alt="girl"
+            />
+          </AuthImage>
+        </BoxForImage>
+        {/* <RegistrationForm /> MIEJSCE NA FORMULARZ */}
+      </Container>
+    </BoxForAuth>
+  );
+}
